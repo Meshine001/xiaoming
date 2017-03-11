a03475aa187016dd71efa14b57202d150f38a4d9
Meshine001 <mingshuai001@hotmail.com>
2017-03-11 21:34:03 +0800

    添加了几个游戏

---
+++ tuijian/games/ChineseChess/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+        <title>中国象棋</title>
+    </head>
+    <style type="text/css">
+    	body{
+    		margin: 0px;
+    		padding: 0px;
+    	}
+    </style>
+   <body>
+	<iframe  id="iframe" src="http://flash.2144.cn/qigongzhu/86/nh0nwhsr/index.htm">
+	</iframe>
+	<script type="text/javascript">
+		window.onload = function() {
+    var iframe = document.getElementById("iframe");
+    iframe.style.width = window.innerWidth + 'px';
+    iframe.style.height = window.innerHeight + 'px';
+};
+	</script>
+    </body>
+</html>
--- xabuss.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$url = "http://www.xajtfb.cn/BusPage/bus_realtime?fromtimeline"; 
-if (isset($url)) 
-{ 
-Header("Location: $url"); 
-} 
-?> 
